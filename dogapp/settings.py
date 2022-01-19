9dfaf92805d6ae6c5a7f7884d010d6f1da2c2181
Adrian <wolfinho7a@gmail.com>
2022-01-19 15:22:01 +0100

    fix(static) : change BASE_DIR  folder

--- dogapp/settings.py
+++ dogapp/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from django.conf import settings
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
